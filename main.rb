b01a5492efd604524c8abe9d0b893b7d105b6130
G90SG <71718345+G90SG@users.noreply.github.com>
2021-01-16 15:58:59 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,10 +1,14 @@
+#input variables
 minutes = ""
 
-
+#asking the user to input the number of minutes
 print "Enter a number of minutes and I will tell you how many hours it is: "
-minutes = gets.to_i 
+#getting user input and converting the string variable to an integer
+minutes = gets.to_i
+#dividing by 60 to get the number of hours
 calc = (minutes).div(60)
+#using modulo to get the remainder when dividing by 60 i.e. the no of minutes
 calc2 = minutes % 60
-
+#returning the results to the user using interpolation
 puts "You entered #{minutes}"\
 " that is #{calc} hours and #{calc2} minutes."
